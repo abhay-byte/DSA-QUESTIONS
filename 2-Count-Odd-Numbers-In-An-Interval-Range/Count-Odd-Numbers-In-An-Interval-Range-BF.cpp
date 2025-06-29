9786707f323b0c150349e32fc8c1a0d49fa22a25
abhay-byte <abhay02delhi@gmail.com>
2025-06-29 06:44:13 +0530

    Runtime: 1672ms ms Memory Usage: 7.83 MB, added comment
    Co-authored-by: Priyanshu Gupta <pg0000742@gmail.com>
    Co-authored-by: REVA <abhay.byte02@gmail.com>

--- 2-Count-Odd-Numbers-In-An-Interval-Range/Count-Odd-Numbers-In-An-Interval-Range-BF.cpp
+++ 2-Count-Odd-Numbers-In-An-Interval-Range/Count-Odd-Numbers-In-An-Interval-Range-BF.cpp
@@ -2,7 +2,17 @@
 #include <vector>
 using namespace std;
 /*
+Time Complexity: O(n)
+Space Complexity: O(1)
 
+Approach: 
+We iterate from the lower bound (low) to the upper bound (high) of the interval.
+For each number in this range, we check if it is odd by using the modulo operator (i % 2 != 0).
+If the number is odd, we increment the count.
+Finally, we return the total count of odd numbers found in the interval.
+
+Result: Runtime: 1672ms ms Memory Usage: 7.83 MB   
+Link: https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/description/
 */
 class Solution {
 public:

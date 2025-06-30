8c6e5c98fe1da299e9edd7d89c895c7d9063b744
abhay-byte <abhay02delhi@gmail.com>
2025-06-30 10:34:02 +0530

    Optimize solution: Runtime 0 ms, Memory 13.39 MB
    Improved performance with optimal implementation.
    
    Co-authored-by: REVA <abhay.byte02@gmail.com>

--- 4-Subtract-the-Product-and-Sum-of-Digits-of-an-Integer/4-Subtract-the-Product-and-Sum-of-Digits-of-an-Integer.cpp
+++ 4-Subtract-the-Product-and-Sum-of-Digits-of-an-Integer/4-Subtract-the-Product-and-Sum-of-Digits-of-an-Integer.cpp
@@ -2,15 +2,34 @@
 #include <vector>
 using namespace std;
 
-/*
-Time: 
-Space: 
-*/
 
+/*
+Time Complexity: O(log(n))
+Space Complexity: O(1)
+
+Approach:
+The function iterates through each digit of the integer n by repeatedly taking the modulus and division by 10.
+It calculates the sum and product of the digits, then returns the difference between the product and the sum.
+
+Result: Runtime: 0 ms Memory Usage: 7.7 MB
+Link: https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+*/
 
 class Solution {
 public:
     int subtractProductAndSum(int n) {
-        
+        int sum = 0; int product = 1;
+
+        while(n!=0)
+        {
+            int digit = n%10;
+            sum += digit;
+            product = product*digit;
+
+            int temp = n/10;
+            n = temp;
+        }
+
+        return product - sum;
     }
 };

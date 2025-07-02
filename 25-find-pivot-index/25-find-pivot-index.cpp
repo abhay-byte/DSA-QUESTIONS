ab76fa5ad5743e69b577fad65cf130867add6eac
abhay-byte <abhay02delhi@gmail.com>
2025-07-02 23:40:41 +0530

    Result: Runtime 0ms Memory 9.10MB
    Co-authored-by: REVA <abhay.byte02@gmail.com>

--- 25-find-pivot-index/25-find-pivot-index.cpp
+++ 25-find-pivot-index/25-find-pivot-index.cpp
@@ -25,6 +25,25 @@ Link: https://leetcode.com/problems/find-pivot-index/
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        
+        int n = nums.size();
+        vector<int> sumLeft(n, 0), sumRight(n, 0);
+
+        // Compute sumLeft
+        for (int i = 1; i < n; ++i) {
+            sumLeft[i] = sumLeft[i - 1] + nums[i - 1];
+        }
+
+        // Compute sumRight
+        for (int i = n - 2; i >= 0; --i) {
+            sumRight[i] = sumRight[i + 1] + nums[i + 1];
+        }
+
+        // Find pivot index
+        for (int i = 0; i < n; ++i) {
+            if (sumLeft[i] == sumRight[i]) {
+                return i;
+            }
+        }
+        return -1;
     }
 };
